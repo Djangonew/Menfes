5fbc3f805ef2ac465f5a78fac6128c0291b96d7c
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-11-03 06:29:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from bot import Bot, data, Database
+from bot import Bot, data, Database,
 
 async def reset_menfess():
     db = Database(data[0])
