e92da52f00d76fd644afc941a92772580c19889e
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-12-04 17:18:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ import os
 
 api_id = int(os.environ.get("API_ID", "20247370"))
 api_hash = os.environ.get("API_HASH", "813309fab8cd9fce260ce7269e543bdb")
-bot_token = os.environ.get("BOT_TOKEN", "6801456940:AAGPL8PMlLxCLbem-i21aGb2Pfu_05KtfoA")
+bot_token = os.environ.get("BOT_TOKEN", "6427697471:AAE61o_mvEw8z-DPi3X8X3iT3Dizg7KjWyc")
 # =========================================================== #
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://Alexa:alexa@cluster0.h0zqfue.mongodb.net/true?retryWrites=true&w=majority")
 db_name = os.environ.get("DB_NAME", "menfess")
 # =========================================================== #
 
-channel_1 = int(os.environ.get("CHANNEL_1", "-1001998341695"))
-channel_2 = int(os.environ.get("CHANNEL_2", "-1002066923405"))
-channel_3 = int(os.environ.get("CHANNEL_3", "-1002132366739"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1001997868124"))
+channel_1 = int(os.environ.get("CHANNEL_1", "-1002030295294"))
+channel_2 = int(os.environ.get("CHANNEL_2", "-1002031228722"))
+channel_3 = int(os.environ.get("CHANNEL_3", "0"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1001710755987"))
 # =========================================================== #
 
-id_admin = int(os.environ.get("ID_ADMIN", "6661774028"))
+id_admin = int(os.environ.get("ID_ADMIN", "1748872441"))
 # =========================================================== #
 
 batas_kirim = int(os.environ.get("BATAS_KIRIM", "5"))
@@ -26,13 +26,13 @@ biaya_kirim = int(os.environ.get("BIAYA_KIRIM", "25"))
 
 # =========================================================== #
 
-hastag = os.environ.get("HASTAG", "#mas #mba #pap #tanya #quote #curhat").replace(" ", "|").lower()
+hastag = os.environ.get("HASTAG", "#kak #abang #pap #tanya #quote #curhat").replace(" ", "|").lower()
 
 # =========================================================== #
 
 pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat diakses harap join terlebih dahulu")
 start_msg = os.environ.get("START_MSG", """"
-Hai {mention} 👋🏻 𝗚𝗝𝗡 𝗔𝘂𝘁𝗼 𝗣𝗼𝘀𝘁 adalah bot auto post pesan yang anda kirim akan masuk ke channel @MENFESS_JAWA silahkan baca help dan rules terlebih dahulu.)
+Hai {mention} 👋🏻 ʟᴘᴍ ᴍᴇɴғᴇs x ɴᴇʀᴏ adalah bot auto post pesan yang anda kirim akan masuk ke channel @lpmnero silahkan baca help dan rules terlebih dahulu.)
 
 gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
 {mention}, pesan mu gagal terkirim. <b>silahkan klik button</b> help bila butuh bantuan.)
