bedc979152e37699c3a0ced39eadf024693d5a56
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-12-05 02:39:53 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -123,7 +123,7 @@ async def help_handler(client, msg):
 #LpmAsk untuk Bertanya
 #LpmStory untuk Berbagi Cerita
 #LpmSpill untuk Spill Masalah
-#LpmFind untuk Mencari Pasangan, Teman, Partner dll"""
+#LpmPap untuk Mengirim pap jangan terlalu brutal sewajarnya!!! (boleh isi dengan kata") dll"""
 
     # Tambahkan InlineKeyboardButton "JASA" di sini
     keyboard = [
